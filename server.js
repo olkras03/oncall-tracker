a66ab511339822762b0f7be5e5c8fe99c2182804
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2020-12-02 17:00:12 +0000

    server can access html file

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const connectDB = require('./DB/Connection');
 const app = express();
+const path = require('path');
 
 connectDB();
 app.use(express.json({ extended: false }));
-app.use('/api/userModel', require('./Api/User'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/userModel', require('./Api/User'));
 
-const Port = process.env.Port || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/users', (req, res) => {
+  res.send('Hellooooooooo!')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html');
+});
+
+const Port = process.env.Port || 3000
 
 app.listen(Port, () => console.log('Server started'));
+//server can connect to the browser
