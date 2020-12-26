294bd839885e634fb82252513bfec575612d262f
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2020-12-26 22:57:40 +0000

    can get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -83,4 +83,18 @@ router.post('/', [auth, [
     }
   });
 
+// @route GET api/profile
+// @desc Get all profiles
+// @access Public
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+})
+
 module.exports = router;
