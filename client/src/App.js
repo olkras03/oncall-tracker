2a15975af7349dea0c11a3270c1c434c84aa13f4
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2021-01-05 23:49:41 +0000

    able to authenticate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/layout/Navbar';
@@ -6,34 +6,45 @@ import Landing from './Components/layout/Landing';
 import Register from './Components/auth/Register';
 import Login from './Components/auth/Login';
 import Alert from './Components/layout/Alert';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 // import Users from './Components/Users';
 // import UserModal from './Components/UserModal';
 
 import { Provider } from 'react-redux';
 import store from './store';
-// import { Container } from 'reactstrap';
 
-const App = () => (
-  <Router>
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, []);
+  return (
+
     <Provider store={store}>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            {/* switch can only have routes */}
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-        {/* <Container>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              {/* switch can only have routes */}
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+          {/* <Container>
           <UserModal />
           <Users />
         </Container> */}
-      </Fragment>
+        </Fragment>
+      </Router>
     </Provider>
-  </Router>
-);
+
+  )
+};
 
 export default App;
