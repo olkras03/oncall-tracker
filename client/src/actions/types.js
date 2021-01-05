9fac549fbacbe02cec20bb0f317d8b13f912bf09
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2021-01-05 23:13:06 +0000

    added authReducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const GET_USERS = 'GET_USERS';
 export const ADD_USER = 'ADD_USER';
 export const DELETE_USER = 'DELETE_USER';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 // import userReducer from './userReducer';
 import alert from './alert';
+import auth from './auth';
 
 export default combineReducers({
     alert,
+    auth
     // user: userReducer
 });
