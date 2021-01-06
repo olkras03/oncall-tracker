6d3fff91a395647d01136b7604d308fcf7f3c763
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2021-01-06 01:32:26 +0000

    able to logout

--- client/src/Components/layout/Navbar.js
+++ client/src/Components/layout/Navbar.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = (props) => {
-    // const [isOpen, setIsOpen] = useState(false);
 
-    // const toggle = () => setIsOpen(!isOpen);
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
+    const authLinks = (
+        <ul>
+            <li>
+                <a onClick={logout} href='#!'>
+                    <i className="fas fa-sign-out-alt" />{' '}
+                    <span className='hide-sm'>Logout</span></a>
+            </li>
+        </ul>
+    );
+
+    const guestLinks = (
+        <ul>
+            <li>
+                <a href='#!'>Developers</a>
+            </li>
+            <li>
+                <Link to='/register'>Register</Link>
+            </li>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+        </ul>
+    );
     return (
         <div>
             <nav className="navbar bg-dark">
@@ -14,30 +38,20 @@ const Navbar = (props) => {
                         <i className="fas fa-code" /> OnCall Tracker
                     </Link>
                 </h1>
-                <ul>
-                    <li>
-                        <Link to='/register'>Register</Link>
-                    </li>
-                    <li>
-                        <Link to='/login'>Login</Link>
-                    </li>
-                </ul>
+                {!loading && (
+                    <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+                )}
             </nav>
-            {/* <Navbar color="light" light expand="sm" className="mb-5">
-                <Container>
-                    <NavbarBrand href="/">OnCall Tracker</NavbarBrand>
-                    <NavbarToggler onClick={toggle} />
-                    <Collapse isOpen={isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="https://github.com/olkras03/oncall-tracker/tree/master">Github</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-            </Navbar> */}
         </div>
     );
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar);
