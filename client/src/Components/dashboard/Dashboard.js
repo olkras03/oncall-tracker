7205f7f709ed1567acba828c656ed567a82ed9e9
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2021-01-20 01:29:44 +0000

    can edit profile

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -18,14 +19,16 @@ const Dashboard = ({
     <p className="lead">
       <i className="fas fa-user" /> Welcome {user && user.name}
     </p>
-    {profile != null ? <Fragment>has</Fragment> : (
-      <Fragment>
-        <p>You have not yet setup a profile, please add some info</p>
-        <Link to="/create-profile" className="btn btn-primary my-1">
-          Create Profile
+    {profile != null ? <Fragment>
+      <DashboardActions />
+    </Fragment> : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
           </Link>
-      </Fragment>
-    )}
+        </Fragment>
+      )}
   </Fragment>
 }
 
--- client/src/Components/profile-forms/EditProfile.js
+++ client/src/Components/profile-forms/EditProfile.js
@@ -1,16 +1,31 @@
-import React, { Fragment, useState, } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
-import { createProfile } from '../../actions/profile';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
-const CreateProfile = ({ createProfile, history }) => {
 
+const EditProfile = ({
+  profile: { profile, loading },
+  createProfile,
+  getCurrentProfile,
+  history
+}) => {
   const [formData, setFormData] = useState({
     company: '',
     status: '',
     skills: '',
   })
+
+  useEffect(() => {
+    getCurrentProfile();
+
+    setFormData({
+      company: loading || !profile.company ? '' : profile.company,
+      status: loading || !profile.status ? '' : profile.status,
+      skills: loading || !profile.skills ? '' : profile.skills.join(',')
+    });
+  }, [getCurrentProfile, loading, profile.company, profile.skills, profile.status]);
 
   const { company, status, skills } = formData;
 
@@ -19,29 +34,29 @@ const CreateProfile = ({ createProfile, history }) => {
 
   const onSubmit = e => {
     e.preventDefault();
-    createProfile(formData, history);
+    createProfile(formData, history, true);
   };
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Create Your Profile</h1>
+      <h1 className="large text-primary">Edit Your Profile</h1>
       <p className="lead">
-        <i className="fas fa-user" /> Add some info to your profile
+        <i className="fas fa-user" /> Add some changes to your profile
       </p>
       <small>* = required field</small>
       <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
           <select name="status" value={status} onChange={onChange}>
             <option>* Select Professional Status</option>
-            <option value="Junior SE">Junior Software Engineer</option>
-            <option value="SE">Software Engineer</option>
+            <option value="Junior Software Engineer">Junior Software Engineer</option>
+            <option value="Software Engineer">Software Engineer</option>
             <option value="Senior Software Engineer">Software Engineer</option>
             <option value="TechLead">Tech Lead</option>
             <option value="Application Architect">Application Architect</option>
-            <option value="SDET">Software Development Engineer in Test</option>
+            <option value="Software Development Engineer in Test">Software Development Engineer in Test</option>
           </select>
           <small className="form-text">
-            Give an idea of where you are at in your career
+            Give us an idea of where you are at in your career
           </small>
         </div>
         <div className="form-group">
@@ -77,10 +92,16 @@ const CreateProfile = ({ createProfile, history }) => {
   )
 }
 
-CreateProfile.propTypes = {
+EditProfile.propTypes = {
   createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-export default connect(null, { createProfile })(
-  withRouter(CreateProfile)
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(
+  withRouter(EditProfile)
 );
