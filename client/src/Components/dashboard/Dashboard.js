c8e2a5985c49734c278d42a63f8fbc50a43104b0
Lena Krasnova <lena.krasnova@bglgroup.co.uk>
2021-04-14 23:50:01 +0100

    Delete button works

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -10,33 +10,36 @@ const Dashboard = ({
   getCurrentProfile,
   deleteAccount,
   auth: { user },
-  profile: { profile, loading } }) => {
+  profile: { profile } }) => {
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
-  return loading && profile === null ? <Spinner /> : <Fragment>
-
-    <h1 className="large text-primary">Dashboard</h1>
-    <p className="lead">
-      <i className="fas fa-user" /> Welcome {user && user.name}
-    </p>
-    {profile != null ? <Fragment>
-      <DashboardActions />
-    </Fragment> : (
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
         <Fragment>
-          <p>You have not yet setup a profile, please add some info</p>
-          <Link to="/create-profile" className="btn btn-primary my-1">
-            Create Profile
-          </Link>
+          <DashboardActions />
           <div className="my-2">
             <button className="btn btn-danger" onClick={() => deleteAccount()}>
               <i className="fas fa-user-minus" /> Delete My Account
             </button>
           </div>
         </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
       )}
-  </Fragment>
-}
+    </Fragment>
+  );
+};
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
--- client/src/Components/profile/Profile.js
+++ client/src/Components/profile/Profile.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -14,7 +16,7 @@ const Profile = ({
     getProfileById(match.params.id);
   }, [getProfileById, match.params.id]);
   return (
-        <Fragment>
+    <Fragment>
       {profile === null ? (
         <Spinner />
       ) : (
@@ -32,37 +34,6 @@ const Profile = ({
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
-            <div className="profile-exp bg-white p-2">
-              <h2 className="text-primary">Experience</h2>
-              {profile.experience.length > 0 ? (
-                <Fragment>
-                  {profile.experience.map((experience) => (
-                    <ProfileExperience
-                      key={experience._id}
-                      experience={experience}
-                    />
-                  ))}
-                </Fragment>
-              ) : (
-                <h4>No experience credentials</h4>
-              )}
-            </div>
-
-            <div className="profile-edu bg-white p-2">
-              <h2 className="text-primary">Education</h2>
-              {profile.education.length > 0 ? (
-                <Fragment>
-                  {profile.education.map((education) => (
-                    <ProfileEducation
-                      key={education._id}
-                      education={education}
-                    />
-                  ))}
-                </Fragment>
-              ) : (
-                <h4>No education credentials</h4>
-              )}
-            </div>
           </div>
         </Fragment>
       )}
